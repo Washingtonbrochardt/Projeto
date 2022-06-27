897de04d9afb14e53e7650833f1a8f7ef5944256
Washington <washingtonbrochardt@hotmail.com>
2022-06-27 17:29:14 -0300

    commit inicial

---
+++ Votacao/Votacao/src/test/java/com/Projeto/Votacao/VotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projeto.Votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
